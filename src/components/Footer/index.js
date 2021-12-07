3f7a2a6deb2c3ffbcf27265fc83c9182c4043df0
marcosspano <e.marcosspano@gmail.com>
2021-12-07 13:24:31 -0300

    Correção da div do Carousel

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import './style.scss';
 export const Footer = () => {
     return (
         <footer>
-            <p>Certified Tech Developer | Digital House Brasil &#127279; Copyleft</p>
+            <p>Certified Tech Developer | Digital House Brasil 	&#127279; Copyleft</p>
         </footer>
     )
 }

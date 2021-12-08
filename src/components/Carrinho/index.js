14079f1de516cd77107cd29b27b9976bb7f57afb
tuliopxavier <tuliopxavier@gmail.com>
2021-12-07 22:48:33 -0300

    🎨 construindo pagina do carrinho

--- src/components/Carrinho/index.js
+++ src/components/Carrinho/index.js
@@ -1,9 +1,48 @@
-import './style.scss'
+import { useState, useEffect } from 'react';
+import api from '../../services/api';
+import './style.scss';
 
 export const Carrinho = () => {
-    return (
-        <div id="carrinho-container">
-            <p>carrinho placeholder</p>
-        </div>
-    )
-}
+  const [productsList, setProductList] = useState([]);
+
+  async function getProducts(quantidade) {
+    try {
+      const response = await api.get('/products');
+      setProductList(response.data.slice(0, quantidade));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getProducts(20);
+  }, []);
+
+  return (
+    <div id='carrinho-container'>
+      <section>
+        {productsList.map(
+          ({ id, title, price, description, category, image }) => {
+            return (
+              <article key={id}>
+                <img src={image} alt='imagem do produto' />
+                <div className='product-container'>
+                  <h3>{title}</h3>
+                  <small>{category}</small>
+                  <p>{description}</p>
+                </div>
+                <div className='product-price'>
+                  <label htmlFor={`qtd-${title}`}>Qtd:</label>
+                  <input type='number' id={`qtd-${title}`} name={`qtd-${title}`} placeholder='1' min='1' max='99' />
+                  <h4><small>R$</small> {price.toFixed(2)}</h4>
+                </div>
+              </article>
+            );
+          }
+        )}
+
+        <div id='total-cart'></div>
+      </section>
+    </div>
+  );
+};
--- src/components/CarrinhoIcon/index.js
+++ src/components/CarrinhoIcon/index.js
@@ -1,13 +1,28 @@
+import { useState, useEffect } from 'react';
+import api from '../../services/api';
 import carrinho from './carrinho.svg';
 import './style.scss';
 
 export const CarrinhoIcon = () => {
-    const carrinhoLength = 2;
+    const [productsList, setProductList] = useState([]);
+
+    async function getProducts(quantidade) {
+        try {
+          const response = await api.get('/products');
+          setProductList(response.data.slice(0, quantidade));
+        } catch (error) {
+          console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        getProducts(20);
+    },[]);
 
     return (
         <div id="carrinho-icon">
             <img src={carrinho} alt="Carrinho de compras" />
-            <p>{carrinhoLength}</p>
+            <p>{productsList.length}</p>
         </div>
     )
 }

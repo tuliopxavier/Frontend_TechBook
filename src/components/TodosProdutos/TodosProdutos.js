5e4c18b5c27273e1f4fddeb1e56dd47179295aeb
giovanispaula <giovannispaula@gmail.com>
2021-12-10 10:40:58 -0300

    Subiu página Todos os Produtos, sem estilizar

--- src/components/TodosProdutos/TodosProdutos.js
+++ src/components/TodosProdutos/TodosProdutos.js
@@ -1,15 +1,22 @@
 import api from '../../services/api'
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
+import { Cards } from '../../components/Cards/index'
 import './style.scss';
 
 
 export const TodosProdutos = () => {
-    const [products, setProducts] = useState()
+    const [products, setProducts] = useState([])
 
-    const getProducts = async () => {
-        const response = await api.get('/products/categories')
-        setProducts(response.data)
+    const getProducts = async (quantidade) => {
+        try {
+            const response = await api.get('/products');
+            setProducts(response.data.slice(0, quantidade))
+        } catch (error) {
+            console.log("Erro")
+        }
+        
+       
 
     }
     useEffect(() => {
@@ -18,21 +25,27 @@ export const TodosProdutos = () => {
 
     console.log(products)
     return(
-
-        <select>
-            {
-            products?.map(
-                ({ id, name }) => {
-                    return (
-                        
-                        <Link to={`/products/categories/${name}`}><option key={id}>{name}</option></Link>
-                    ) 
-                            
-
-                }
-            )
+       <> 
+        <div id='produtos-container'>
+        <section>
+          {products.map(
+              ({ id, title, price, description, category, image }) => {
+                  return (
+                  <Link to={`/products/${id}`}>
+                <article key={id}>
+                  <img src={image} alt='imagem do produto' />
+                  <div className='product-container'>
+                    <h3>{title}</h3>
+                    {/* <small>{category}</small> */}
+                  </div>
+                </article>
+              </Link>
+              );
             }
-        </select>
+          )}
+      </section>
+    </div>
+    </>
     )
     
 }

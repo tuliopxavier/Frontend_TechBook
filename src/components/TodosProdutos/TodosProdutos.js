e2c2fa80666ad8c9df2283c104d8ed160df7682e
giovanispaula <giovannispaula@gmail.com>
2021-12-10 14:50:41 -0300

    Todos os produtos pronta e estilizada.

--- src/components/TodosProdutos/TodosProdutos.js
+++ src/components/TodosProdutos/TodosProdutos.js
@@ -1,45 +1,55 @@
 import api from '../../services/api'
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
-import { Cards } from '../../components/Cards/index'
 import './style.scss';
 
 
 export const TodosProdutos = () => {
     const [products, setProducts] = useState([])
 
-    const getProducts = async (quantidade) => {
-        try {
-            const response = await api.get('/products');
-            setProducts(response.data.slice(0, quantidade))
-        } catch (error) {
-            console.log("Erro")
-        }
-        
-       
 
+    const getProducts = async (quantidade) => {
+          try {
+              const response = await api.get('/products');
+              setProducts(response.data.slice(0, quantidade))
+          } catch (error) {
+              console.log("Erro produtos")
+          }
     }
+    
     useEffect(() => {
         getProducts()
     }, [])
 
-    console.log(products)
+  
+
     return(
        <> 
         <div id='produtos-container'>
         <section>
+          <div id="menu-container">
+            <h3>Categorias: </h3>
+            {category.map(
+              ({id, category}) =>{
+                return(
+                  <h4 key={category}> {category} </h4>
+                )
+              }
+            )}
+            
+     
+          </div>
           {products.map(
-              ({ id, title, price, description, category, image }) => {
+              ({ id, title, image }) => {
                   return (
-                  <Link to={`/products/${id}`}>
-                <article key={id}>
-                  <img src={image} alt='imagem do produto' />
-                  <div className='product-container'>
-                    <h3>{title}</h3>
-                    {/* <small>{category}</small> */}
-                  </div>
-                </article>
-              </Link>
+                    <article key={id}>
+                      <Link to={`/products/${id}`}>
+                        <img src={image} alt='imagem do produto' />
+                        <div className='product-container'>
+                          <h3>{title}</h3>
+                        </div>
+                      </Link>
+                    </article>
               );
             }
           )}
